390fa35e9bd6ee9ead9b1c388840ef8b60cb50ad
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-02 11:26:11 +0300

    initial commit
    Fresh beginning

---
+++ src/test/java/io/github/alprkeskin/openexchangeratesmicroservice/OpenExchangeRatesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.openexchangeratesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenExchangeRatesMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
