5fd218c9ee50ae9de138bb850dcfc3032ff6f974
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-07 16:17:30 +0300

    Logger
    - used slf4j logger instead of java.util.Logger

--- src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/rest/ServiceController.java
+++ src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/rest/ServiceController.java
@@ -3,13 +3,13 @@ package io.github.alprkeskin.openexchangeratesmicroservice.rest;
 import io.github.alprkeskin.openexchangeratesmicroservice.model.CurrencyRates;
 import io.github.alprkeskin.openexchangeratesmicroservice.service.OpenExchangeRatesMainService;
 import io.github.alprkeskin.openexchangeratesmicroservice.utils.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /** The methods of this class automatically run according to the given path when a http request is sent **/
 @RestController
@@ -18,14 +18,14 @@ public class ServiceController {
     @Autowired
     private OpenExchangeRatesMainService openExchangeRatesMainService;
 
-    private static final Logger logger = Logger.getLogger(ServiceController.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(ServiceController.class);
 
     @GetMapping(value = {"", "/{requestedDate}"})
     public ResponseEntity<CurrencyRates> getResponse(
             @PathVariable(value = "requestedDate", required = false) String requestedDate,
             @RequestParam(value = "symbols", required = false, defaultValue = "XXX") String symbols) {
 
-        logger.log(Level.INFO, "ServiceController::getResponse");
+        logger.info("ServiceController::getResponse");
         LocalDate desiredDate = requestedDate == null ? LocalDate.now(): DateUtil.getLocalDate(requestedDate);
         return openExchangeRatesMainService.getResponse(desiredDate, symbols);
     }
