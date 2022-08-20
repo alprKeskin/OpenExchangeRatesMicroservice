572ea9e3952424e04c3071a7e953c595e80b0ef0
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-20 15:17:13 +0300

    Update OpenExchangeRatesMicroserviceApplication.java
    - move scheduling class as a separate class

--- src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/OpenExchangeRatesMicroserviceApplication.java
+++ src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/OpenExchangeRatesMicroserviceApplication.java
@@ -1,17 +1,9 @@
 package io.github.alprkeskin.openexchangeratesmicroservice;
 
-import io.github.alprkeskin.openexchangeratesmicroservice.service.OpenExchangeRatesMainService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
-import java.time.LocalDate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableScheduling
@@ -19,23 +11,4 @@ public class OpenExchangeRatesMicroserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OpenExchangeRatesMicroserviceApplication.class, args);
     }
-
-
-    private final Logger logger = Logger.getLogger("OpenExchangeRatesMicroserviceApplication.class");
-
-    @Autowired
-    private OpenExchangeRatesMainService openExchangeRatesMainService;
-
-    @Scheduled(cron = "*/10 * * * * *")
-    public void getDailyRates() {
-        logger.log(Level.INFO, "Scheduled getDailyRates...");
-        openExchangeRatesMainService.getResponse(LocalDate.now(), "TRY,EUR,USD");
-    }
-}
-
-
-@Configuration
-@EnableScheduling
-@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
-class SchedulingConfiguration {
 }
