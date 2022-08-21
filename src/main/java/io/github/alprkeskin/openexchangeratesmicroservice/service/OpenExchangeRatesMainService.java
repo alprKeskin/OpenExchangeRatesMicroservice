67071f0af41e34bb5f25d444179c91cb2cca159b
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-21 16:52:06 +0300

    Update OpenExchangeRatesMainService.java
    - removed unnecessary comments
    - optimized return types of methods
    - added a special constructor

--- src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/service/OpenExchangeRatesMainService.java
+++ src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/service/OpenExchangeRatesMainService.java
@@ -3,13 +3,12 @@ package io.github.alprkeskin.openexchangeratesmicroservice.service;
 
 import io.github.alprkeskin.openexchangeratesmicroservice.model.CurrencyRates;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Optional;
 
-import static org.springframework.http.ResponseEntity.ok;
 
 @Service
 public class OpenExchangeRatesMainService {
@@ -19,23 +18,23 @@ public class OpenExchangeRatesMainService {
     @Autowired
     private CurrencyPersistanceService currencyPersistanceService;
 
-    public ResponseEntity<CurrencyRates> getResponse(LocalDate date, String symbols) {
+    OpenExchangeRatesMainService(ExchangeRatesService exchangeRatesService, CurrencyPersistanceService currencyPersistanceService) {
+        this.exchangeRatesService = exchangeRatesService;
+        this.currencyPersistanceService = currencyPersistanceService;
+    }
 
-        // try to take the currency rates of the related date from database
+    public CurrencyRates getCurrencyRates(LocalDate date, String symbols) {
         Optional<CurrencyRates> currencyRates = currencyPersistanceService.findCurrencyRatesById(date);
-        // if there is no any current rates object of the related date in the database
         if (currencyRates.isEmpty()) {
-            // pull the current rates of related date from open exchange rates website
             CurrencyRates currencyRatesOfRelatedDate = exchangeRatesService.getCurrencyRates(date, symbols);
-            // save related date's current rates to the database
             currencyPersistanceService.saveCurrencyRates(currencyRatesOfRelatedDate, date);
-            // give a response to the user
-            return ok(currencyRatesOfRelatedDate);
+            return currencyRatesOfRelatedDate;
         }
-        // if there exist a current rates object of related date in the database
-        // return the currency rates object of related date from the database
-        // currencyRates.get() ==> currencyRate without optional
-        return ok(currencyRates.get());
+        return currencyRates.get();
+    }
+
+    public Map getCurrencies() {
+        return exchangeRatesService.getCurrencies();
     }
 
 }
