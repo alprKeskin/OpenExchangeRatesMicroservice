96537f54f56bec937ed59bf6c13c3aa023631bf1
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-08-22 16:49:19 +0300

    Unnecessary Logs
    - unnecessary logs has been removed

--- src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/rest/ServiceController.java
+++ src/main/java/io/github/alprkeskin/openexchangeratesmicroservice/rest/ServiceController.java
@@ -3,13 +3,12 @@ package io.github.alprkeskin.openexchangeratesmicroservice.rest;
 import io.github.alprkeskin.openexchangeratesmicroservice.model.CurrencyRates;
 import io.github.alprkeskin.openexchangeratesmicroservice.service.OpenExchangeRatesMainService;
 import io.github.alprkeskin.openexchangeratesmicroservice.utils.DateUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 /** The methods of this class automatically run according to the given path when a http request is sent **/
 @RestController
@@ -19,20 +18,18 @@ public class ServiceController {
     @Autowired
     private OpenExchangeRatesMainService openExchangeRatesMainService;
 
-    /**
-     * org.slf4j.Logger is an interface
-     * So, we coded to interface (good approach)
-     **/
-    private final static Logger logger = LoggerFactory.getLogger(ServiceController.class);
-
     @GetMapping(value = {"", "/{requestedDate}"})
-    public ResponseEntity<CurrencyRates> getResponse(
+    public ResponseEntity<CurrencyRates> getCurrencyRates(
             @PathVariable(value = "requestedDate", required = false) String requestedDate,
             @RequestParam(value = "symbols", required = false, defaultValue = "XXX") String symbols) {
 
-        logger.info("ServiceController::getResponse");
-        LocalDate desiredDate = requestedDate == null ? LocalDate.now(): DateUtil.getLocalDate(requestedDate);
-        return openExchangeRatesMainService.getResponse(desiredDate, symbols);
+        LocalDate desiredDate = requestedDate == null ? LocalDate.now() : DateUtil.getLocalDate(requestedDate);
+        return ResponseEntity.ok(openExchangeRatesMainService.getCurrencyRates(desiredDate, symbols));
+    }
+
+    @GetMapping(value = "currency/currencies")
+    public ResponseEntity<Map> getCurrencies() {
+        return ResponseEntity.ok(openExchangeRatesMainService.getCurrencies());
     }
 }
 
